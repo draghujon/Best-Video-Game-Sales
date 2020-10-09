5948dcd1dcae730fc910947be182df212bd5523f
Chris Feasby <cfeasby204@gmail.com>
2020-10-09 15:45:44 -0500

    Updated scaffolds to make Bulma work after doing the Scaffold Page generate

--- app/models/game_platform.rb
+++ app/models/game_platform.rb
@@ -3,4 +3,6 @@ class GamePlatform < ApplicationRecord
   belongs_to :platform
   has_many :region_sales, dependent: :destroy
   has_many :regions, through: :region_sales
+
+  validates :release_year, numericality: { only_integer: true }
 end
